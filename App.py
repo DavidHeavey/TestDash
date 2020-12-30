99efece9b11cff87e33a3de193ffcd8f89fdb5e4
DavidHeavey <64201109+DavidHeavey@users.noreply.github.com>
2020-12-30 16:05:09 +0000

    update

--- App.py
+++ App.py
@@ -4,11 +4,12 @@ import dash_html_components as html
 from dash.dependencies import Output, Input
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import datetime
 
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
 
 # read data
 headers = ['Date', 'Count']
@@ -17,15 +18,13 @@ df_20 = pd.read_csv('Data/2020.csv', names=headers, header=None, nrows=366, pars
 df_19 = pd.read_csv('Data/2019.csv', names=headers, header=None, nrows=365, parse_dates=[0])
 df_18 = pd.read_csv('Data/2018.csv', names=headers, header=None, nrows=365, parse_dates=[0])
 df_17 = pd.read_csv('Data/2017.csv', names=headers, header=None, nrows=365, parse_dates=[0])
-df_16 = pd.read_csv('Data/2016.csv', names=headers, header=None, nrows=365, parse_dates=[0])
+df_16 = pd.read_csv('Data/2016.csv', names=headers, header=None, nrows=366, parse_dates=[0])
 df_r = pd.read_csv('Data/Restrictions_2020.csv', sep=',', names=['Date', 'Restriction'], header=None, nrows=365, parse_dates=[0])
+df_all = pd.concat([df_19, df_18, df_17, df_16], axis=0)
 
 print(df_19.head())
 print(df_20.Date.dtype)
 print(df_20)
-
-fig = px.line(df_19, x='Date', y='Count', title='Traffic Counts')
-fig.update_xaxes(dtick='M1', tickformat='%b', ticklabelmode='period')
 
 # styling
 SIDEBAR_STYLE_DICT = {
@@ -35,7 +34,7 @@ SIDEBAR_STYLE_DICT = {
     'bottom': 0,
     'width': '16rem',
     'padding': '2rem 1rem',
-    'background-color': '#f8f9fa',
+    #'background-color': '#f8f9fa',
 }
 
 CONTENT_STYLE_DICT = {
@@ -43,12 +42,15 @@ CONTENT_STYLE_DICT = {
     'margin-right': '2rem',
     'padding': '2rem 1rem',
 }
-# logo
-# image_card = dbc.Card(
-#     [
-#         dbc.CardImg(src='/Data/AIT_logo.png')
-#     ]
-# )
+
+fig1 = px.line(df_all, x='Date', y='Count', title='Traffic Counts')
+fig1.update_xaxes(rangeslider_visible=True)
+#fig1.update_xaxes(dtick='M1', tickformat='%b', ticklabelmode='period')
+
+fig2 = go.Figure()
+fig2.add_trace(go.Scatter(data=df_19, x='Date', y='Count'))
+#fig2.update_xaxes(dtick='M1', tickformat='%b', ticklabelmode='period')
+
 
 sidebar = dbc.Card([
     # dbc.CardBody([
@@ -59,9 +61,22 @@ sidebar = dbc.Card([
     dbc.CardBody([
         html.P('Select a page:'),
         html.Hr(),
+        dbc.Nav(
+        [
+            dbc.NavLink("Home", active='exact', href="/", className='my-2'),
+            dbc.NavLink("Compare Years", active='exact', href="/compare", className='my-2'),
+            dbc.NavLink("Select Dates", active='exact', href="/select"),
+        ],
+        pills=True, card=True, vertical=True, className='mx-1'
+        ),
     ]),
-    dbc.Button('Click',id='samplebutton', className='mr-2'),
-], color='dark', style={'height':'100vh',
+    dbc.CardBody([
+        html.Hr(),
+        html.H6('Applied Scripting Languages', className='font-weight-bold'),
+        html.Hr(),
+        html.P('Student ID: A00279257'),
+    ]),
+], color='light', style={'height':'100vh',
                          'width':'16rem', 'position':'fixed'})
 
 content = html.Div(id='page-content', children=[], style=CONTENT_STYLE_DICT)
@@ -73,6 +88,28 @@ app.layout = html.Div([
     sidebar,
     content
 ])
+
+@app.callback(
+    Output('page-content', 'children'),
+    [Input('url', 'pathname')]
+)
+def render_page(pathname):
+    if pathname == '/':
+        return [
+            html.H1('Athlone Traffic Counts'),
+            dcc.Graph(id='plot', figure=fig1)
+        ]
+    elif pathname == '/compare':
+        return [
+            html.H1('Yearly Traffic Comparison'),
+            dcc.Graph(id='plot', figure=fig2)
+        ]
+    elif pathname == '/select':
+        return [
+            html.H1('Select Count Date range'),
+            dcc.Graph(id='plot', figure=px.line(df_19, x='Date', y='Count', title='Traffic Counts'))
+        ]
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
